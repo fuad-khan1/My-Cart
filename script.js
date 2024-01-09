b03cf131a19dab10dd448c91b21db66cd5be9658
Fuad Khan <fuadkhanantor1@gmail.com>
2024-01-10 04:17:54 +0600

    delete, add to order, cart icon functionality updated

--- script.js
+++ script.js
@@ -7,21 +7,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeCartButton = document.getElementById("close-cart");
   const cart = [];
 
+
+// -----------    Data Fetching   -----------  // 
   fetch("data.json")
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((product) => {
-        const card = document.createElement("div");
-        card.className = "p-3 max-w-xs rounded bg-gray-100";
-        card.innerHTML = `
+        data.forEach((product) => {
+          const card = document.createElement("div");
+          card.className = "p-3 max-w-xs rounded bg-gray-100";
+          card.innerHTML = `
             <img src="${product.thumbnail}" class="w-full" alt="${product.title}">
             <h1 class="font-bold text-xl pt-3">${product.title}</h1>
             <p class="text-gray-700">$${product.price}</p>
             <p class="text-gray-600 py-3">${product.description}</p>
-            <button onclick="addToOrder('${product.title}', ${product.price}, '${product.image}')" class="w-full text-white bg-red-500 py-2 px-4 rounded text-sm font-semibold mb-2">Add to order</button>
+            <button id="add-to-order-${product.id}" onclick="addToOrder('${product.title}', ${product.price}, '${product.image}','add-to-order-${product.id}', ${product.id})" class="w-full text-white bg-red-500 py-2 px-4 rounded text-sm font-semibold mb-2">Add to order</button>
             <button class="w-full text-red-400 py-2 px-4 rounded text-sm font-semibold border border-2 border-red-400">Customize</button>
           `;
-        productSection.appendChild(card);
+          productSection.appendChild(card);
       });
     });
 
@@ -30,20 +32,29 @@ document.addEventListener("DOMContentLoaded", () => {
     cartButton.innerHTML = `<sup class="bg-red-500 text-white p-1 rounded text-xs">${cartCount}</sup>`;
   }
 
-  window.addToOrder = function addToOrder(itemName, price, image) {
+  window.addToOrder = function addToOrder(itemName, price, image, buttonId, productId) {
     const existingItem = cart.find((item) => item.name === itemName);
 
     if (existingItem) {
       existingItem.quantity += 1;
     } else {
-      cart.push({ name: itemName, price: price, quantity: 1, image: image });
+      cart.push({id: productId, name: itemName, price: price, quantity: 1, image: image });
     }
 
     updateCartSidebar();
     showCartSidebar();
     updateCartIcon();
+
+    const addButton = document.getElementById(buttonId);
+  if (addButton) {
+    addButton.disabled = true;
+    addButton.className= "w-full text-white bg-gray-500 py-2 px-4 rounded text-sm font-semibold mb-2"
+    addButton.innerHTML= `Added to Cart`
+  }
   };
 
+
+  // -----------   updateCartSidebar function   -----------  // 
   function updateCartSidebar() {
     cartItemsList.innerHTML = "";
 
@@ -55,21 +66,65 @@ document.addEventListener("DOMContentLoaded", () => {
           <div>
             <img src="${item.image}" class="w-20 h-20 rounded" alt="${item.name} image">
           </div>
-          <div class="ml-4 text-white">
-            <p class=" font-medlium">${item.name} x${item.quantity}</p>
-            <p class="text-xs">$${item.price}</p>
+          <div class="ml-3 text-white">
+            <p class=" font-medlium">${item.name}</p>
+            <p class="text-xs mb-2">$${item.price}</p>
+            <button onclick="minusQuantity(${index})" class= "bg-gray-200 text-black px-1 rounded">-</button>
+            <span class="bg-white px-3 text-black">${item.quantity}</span>
+            <button onclick="plusQuantity(${index})" class= "bg-gray-200 text-black px-1 rounded">+</button>
+
           </div>
         </div>
-        <button class="absolute top-0 right-0 text-red-500 bg-white p-1 rounded text-xs cursor-pointer" onclick="removeItem(${index})">
+        
+        <button class="absolute top-0 right-0 text-red-500 bg-white p-1 rounded text-xs cursor-pointer" onclick="deleteItem(${index})">
           <i class="fas fa-trash"></i>
         </button>
+        
       </div>
         `;
+        
       cartItemsList.appendChild(listItem);
     });
 
-    totalPriceElement.innerHTML = `Total Price: $${calculateTotalPrice()}`;
+    totalPriceElement.innerHTML = `Total: $${calculateTotalPrice()}`;
   }
+
+  window.minusQuantity= function minusQuantity(index) {
+    const existing = cart[index];
+  
+    if (existing && existing.quantity > 1) {
+        existing.quantity -= 1;
+    }
+  
+    updateCartSidebar();
+  }
+  
+  window.plusQuantity= function plusQuantity(index) {
+    const existing = cart[index];
+  
+    if (existing) {
+        existing.quantity += 1;
+    }
+  
+    updateCartSidebar();
+  }
+
+
+  // -----------   Delete item function   -----------  // 
+  window.deleteItem= function deleteItem(index) {
+    const deletedItem = cart[index];
+    cart.splice(index, 1);
+    updateCartSidebar();
+    updateCartIcon();
+
+    const addButton = document.getElementById(`add-to-order-${deletedItem.id}`);
+    if (addButton) {
+      addButton.disabled = false;
+      addButton.className = "w-full text-white bg-red-500 py-2 px-4 rounded text-sm font-semibold mb-2";
+      addButton.innerHTML = "Add to Order";
+    }
+  }
+  
 
   function calculateTotalPrice() {
     return cart.reduce((total, item) => total + item.price * item.quantity, 0);
